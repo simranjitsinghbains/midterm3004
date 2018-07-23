12db498689a22bc55dfd5e2466ba74014f91dc55
MacStudent <macstudent@20x-imacxx-m-374.local>
2018-07-23 11:32:51 -0400

    Initial Commit

---
+++ C0736030_Exam1_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0736030_Exam1_MAD3004
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
